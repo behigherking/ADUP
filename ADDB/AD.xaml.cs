2fb43f04294f002efa16c961c9dce9be5803a4d0
behigherking <korolkovdemid@mail.ru>
2025-01-29 01:41:42 +0300

    Добавьте файлы проекта.

---
+++ ADDB/AD.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ADDB
+{
+    public partial class AD : Page
+    {
+        public AD()
+        {
+            CategoryComboBox.ItemsSource = Entities.GetContext().AdCategory.ToList();
+            CityFilter.ItemsSource = Entities.GetContext().City.ToList();
+            StatusFilter.ItemsSource = Entities.GetContext().AdType.ToList();
+            UpdateAds(); // Начальная загрузка объявлений
+        }
+
+        private void UpdateAds()
+        {
+            var currentAds = Entities.GetContext().Advertisement.ToList();
+
+            // Фильтр по ключевым словам
+            if (!string.IsNullOrEmpty(SearchTextBox.Text))
+            {
+                currentAds = currentAds.Where(p => p.Title.ToLower().Contains(SearchTextBox.Text.ToLower())).ToList();
+            }
+
+            // Фильтр по категории
+            if (CategoryComboBox.SelectedValue != null)
+            {
+                int selectedCategoryId = (int)CategoryComboBox.SelectedValue;
+                currentAds = currentAds.Where(p => p.CategoryID == selectedCategoryId).ToList();
+            }
+
+            // Фильтр по городу
+            if (CityFilter.SelectedValue != null)
+            {
+                int selectedCityId = (int)CityFilter.SelectedValue;
+                currentAds = currentAds.Where(p => p.CityID == selectedCityId).ToList();
+            }
+
+            // Фильтр по статусу
+            if (StatusFilter.SelectedValue != null)
+            {
+                int selectedTypeId = (int)StatusFilter.SelectedValue;
+                currentAds = currentAds.Where(p => p.TypeID == selectedTypeId).ToList();
+            }
+
+            // Радиокнопки для активных/завершенных объявлений
+            if (activeRb.IsChecked == true)
+            {
+                currentAds = currentAds.Where(p => p.StatusID == 1).ToList();
+            }
+            else if (finishedRb.IsChecked == true)
+            {
+                currentAds = currentAds.Where(p => p.StatusID == 2).ToList();
+            }
+
+            ADListBox.ItemsSource = currentAds; // Привязка данных к ListBox
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAds(); // Обновление списка объявлений
+        }
+
+        private void CategoryComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAds();
+        }
+
+        private void CityFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAds();
+        }
+
+        private void StatusFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAds();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateAds();
+        }
+    }
+}
---
+++ ADDB/Login.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADDB
+{
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text;
+            string password = PasswordBox.Password;
+
+            if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Логин и пароль не могут быть пустыми.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+        private void authButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(LoginTextBox.Text) || string.IsNullOrEmpty(PasswordBox.Password))
+            {
+                MessageBox.Show("Введите логин и пароль!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            using (var db = new Entities())
+            {
+                var user = db.Users.AsNoTracking().FirstOrDefault(p => p.Login == LoginTextBox.Text && p.Password == PasswordBox.Password);
+
+                if (user == null)
+                {
+                    MessageBox.Show("Пользователь с такими данными не найден!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    NavigationService.Navigate(new User());
+                }
+            }
+        }
+    }
+}
