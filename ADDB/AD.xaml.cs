ffcd8b50bac6f7429d86f7c3df7943579bab86bb
behigherking <korolkovdemid@mail.ru>
2025-01-29 17:25:22 +0300

    починить

--- ADDB/AD.xaml.cs
+++ ADDB/AD.xaml.cs
@@ -14,52 +14,93 @@ namespace ADDB
             CategoryComboBox.ItemsSource = Entities.GetContext().AdCategory.ToList();
             CityFilter.ItemsSource = Entities.GetContext().City.ToList();
             StatusFilter.ItemsSource = Entities.GetContext().AdType.ToList();
-            UpdateAds(); 
+            listview.ItemsSource = Entities.GetContext().Advertisement.ToList();
+            UpdateAds();
         }
 
+        /*        private void UpdateAds()
+                {
+                    var currentAds = Entities.GetContext().Advertisement.ToList();
+
+                    // Фильтр по ключевым словам
+
+                    currentAds = currentAds.Where(p => p.Title.ToLower().Contains(SearchTextBox.Text.ToLower())).ToList();
+
+
+                    // Фильтр по категории
+                    if (CategoryComboBox.SelectedValue != null)
+                    {
+                        int selectedCategoryId = (int)CategoryComboBox.SelectedValue;
+                        currentAds = currentAds.Where(p => p.CategoryID == selectedCategoryId).ToList();
+                    }
+
+                    // Фильтр по городу
+                    if (CityFilter.SelectedValue != null)
+                    {
+                        int selectedCityId = (int)CityFilter.SelectedValue;
+                        currentAds = currentAds.Where(p => p.CityID == selectedCityId).ToList();
+                    }
+
+                    // Фильтр по статусу
+                    if (StatusFilter.SelectedValue != null)
+                    {
+                        int selectedTypeId = (int)StatusFilter.SelectedValue;
+                        currentAds = currentAds.Where(p => p.TypeID == selectedTypeId).ToList();
+                    }
+
+                    // Радиокнопки для активных/завершенных объявлений
+                    if (activeRb.IsChecked == true)
+                    {
+                        currentAds = currentAds.Where(p => p.StatusID == 1).ToList();
+                    }
+                    else if (finishedRb.IsChecked == true)
+                    {
+                        currentAds = currentAds.Where(p => p.StatusID == 2).ToList();
+                    }
+
+                    ADList.ItemsSource = currentAds; // Привязка данных к ListBox
+                }*/
         private void UpdateAds()
         {
             var currentAds = Entities.GetContext().Advertisement.ToList();
 
-            // Фильтр по ключевым словам
-            if (!string.IsNullOrEmpty(SearchTextBox.Text))
-            {
-                currentAds = currentAds.Where(p => p.Title.ToLower().Contains(SearchTextBox.Text.ToLower())).ToList();
-            }
+            // Поиск по наименованию
+            currentAds = currentAds.Where(p => p.Title.ToLower().Contains(SearchTextBox.Text.ToLower())).ToList();
 
-            // Фильтр по категории
+
+
             if (CategoryComboBox.SelectedValue != null)
             {
                 int selectedCategoryId = (int)CategoryComboBox.SelectedValue;
                 currentAds = currentAds.Where(p => p.CategoryID == selectedCategoryId).ToList();
             }
 
-            // Фильтр по городу
             if (CityFilter.SelectedValue != null)
             {
                 int selectedCityId = (int)CityFilter.SelectedValue;
                 currentAds = currentAds.Where(p => p.CityID == selectedCityId).ToList();
             }
 
-            // Фильтр по статусу
             if (StatusFilter.SelectedValue != null)
             {
                 int selectedTypeId = (int)StatusFilter.SelectedValue;
                 currentAds = currentAds.Where(p => p.TypeID == selectedTypeId).ToList();
             }
 
-            // Радиокнопки для активных/завершенных объявлений
             if (activeRb.IsChecked == true)
             {
                 currentAds = currentAds.Where(p => p.StatusID == 1).ToList();
             }
-            else if (finishedRb.IsChecked == true)
+            else
             {
                 currentAds = currentAds.Where(p => p.StatusID == 2).ToList();
             }
 
-            ADList.ItemsSource = currentAds; // Привязка данных к ListBox
+
+
+            listview.ItemsSource = currentAds;
         }
+
 
         private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -95,7 +136,7 @@ namespace ADDB
         }
         private void ADList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            var selectedAd = (Advertisement)ADList.SelectedItem;
+            var selectedAd = (Advertisement)listview.SelectedItem;
             if (selectedAd != null)
             {
                 NavigationService.Navigate(new EditAdPage(selectedAd));
